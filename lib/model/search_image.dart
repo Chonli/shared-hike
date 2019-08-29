72819cfc1105ba96dd64028ec2bc117af07c81de
Chonli <Chonli@users.noreply.github.com>
2019-08-29 23:58:17 +0200

    decode result search

--- lib/model/search_image.dart
+++ lib/model/search_image.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 class SearchImage {
   final urlAPI = "https://pixabay.com/api/?";
-  final _key = "<add key here>";
+  final _key = "";
   static final SearchImage _singleton = SearchImage._internal();
 
   SearchImage._internal();
@@ -13,17 +13,25 @@ class SearchImage {
   }
 
   Future<List<String>> searchImages(String query) async {
-    //final _query = query.replaceAll(' ', '+');
-    var client = new http.Client();    
-    var response = await client.get(urlAPI + "key=$_key&q=$query&image_type=photo");
+    if(query.isNotEmpty) {
+      final _query = query.replaceAll(' ', '+');
+      var client = new http.Client();
+      print("Search : " + urlAPI + "key=$_key&q=$_query&image_type=photo");
+      var response = await client.get(
+          urlAPI + "key=$_key&q=$_query&image_type=photo");
 
-    if (response.statusCode == 200) {
-      var jsonResponse = convert.jsonDecode(response.body);
-      print(jsonResponse);
-      return [];
-    } else {
-      print("searchImages error ${response.statusCode}");
-      return [];
+      if (response.statusCode == 200) {
+        var jsonResponse = convert.jsonDecode(response.body);
+        List<String> result = [];
+        for (var hit in jsonResponse['hits']) {
+          result.add(hit['webformatURL']);
+        }
+
+        return result;
+      } else {
+        print("searchImages error ${response.statusCode}");
+      }
     }
+    return [];
   }
 }
--- lib/ui/search_image_page.dart
+++ lib/ui/search_image_page.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_hike/model/search_image.dart';
 
@@ -13,6 +14,7 @@ class SearchImagePage extends StatefulWidget {
 class _SearchImagePageState extends State<SearchImagePage> {
   final SearchImage searchImage = SearchImage();
   final TextEditingController _filter = TextEditingController();
+  String _mQuery = "";
 
   @override
   void initState() {
@@ -22,19 +24,52 @@ class _SearchImagePageState extends State<SearchImagePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: TextField(
-        controller: _filter,
-        decoration: InputDecoration(
-          prefixIcon: Icon(Icons.search),
-          hintText: 'Recherche Image...',
+      appBar: AppBar(
+        title: TextField(
+          controller: _filter,
+          decoration: InputDecoration(
+            prefixIcon: Icon(Icons.search),
+            hintText: 'Recherche Image...',
+          ),
+          onSubmitted: (query) {
+            if (_mQuery != query || query != "") {
+              setState(() {
+                _mQuery = query;
+              });
+            }
+          },
         ),
-        onSubmitted: (query) {
-          print("query=$query");
-          searchImage.searchImages(query);
-        },
       ),
-    ));
+      body: FutureBuilder(
+          future: SearchImage().searchImages(_mQuery),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(child: CircularProgressIndicator());
+              default:
+                if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  if (snapshot.data.length > 0) {
+                    return GridView.builder(
+                        itemCount: snapshot.data.length,
+                        gridDelegate:
+                            new SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2),
+                        itemBuilder: (context, index) {
+                          return GridTile(
+                              child: CachedNetworkImage(
+                            imageUrl: snapshot.data[index],
+                          ));
+                        });
+                  } else {
+                    return Container();
+                  }
+                }
+            }
+          }),
+    );
   }
 }
 
